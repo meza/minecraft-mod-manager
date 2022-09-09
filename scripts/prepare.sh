4c0a2342b311a67767b8725fb6bd231a293d7cd7
meza <meza@meza.hu>
2022-09-09 21:49:06 +0100

    fix: fixed the sed issue

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -8,5 +8,5 @@ export const version = '$VERSION';
 EOF
 
 sed -i "s/\"version\": \"[0-9.]*\"/\"version\": \"$VERSION\"/" package.json
-sed -i "s/REPL_CURSEFORGE_API_KEY/$CURSEFORGE_API_KEY/" src/env.ts
-sed -i "s/REPL_MODRINTH_API_KEY/$MODRINTH_API_KEY/" src/env.ts
+sed -i "s|REPL_CURSEFORGE_API_KEY|$CURSEFORGE_API_KEY|" src/env.ts
+sed -i "s|REPL_MODRINTH_API_KEY|$MODRINTH_API_KEY|" src/env.ts
