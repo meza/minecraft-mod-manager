cd68e4892e35b4ec4562d2f82ee3f55d3a374d2d
meza <meza@meza.hu>
2022-09-11 01:21:02 +0100

    test: added unit test reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    cache: false,
+    cache: {
+      dir: '.cache/.vitest'
+    },
     dir: 'src',
     testTimeout: 10000,
     watch: false,
+    outputFile: 'reports/junit.xml',
+    reporters: ['default', 'junit'],
     coverage: {
       reportsDirectory: './reports/coverage/unit',
-      reporter: ['text', 'json', 'html']
-    },
-    globalSetup: './test/globalSetup.ts'
+      reporter: ['text', 'json', 'html'],
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100
+    }
   }
 });
