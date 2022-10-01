77cfa87defaeabd0531ca4b4fe6b3bfa8360ea59
meza <meza@meza.hu>
2022-10-01 18:14:29 +0100

    chore: covered the env variables

---
+++ src/env.test.ts
@@ -0,0 +1,21 @@
+import { afterEach, describe, expect, it, vi } from 'vitest';
+
+describe('The environment variables', () => {
+  afterEach(() => {
+    vi.resetModules();
+  });
+
+  it('are set to their defaults when they are not overridden', async () => {
+    const { curseForgeApiKey, modrinthApiKey } = await import('./env.js');
+    expect(curseForgeApiKey).toBe('REPL_CURSEFORGE_API_KEY');
+    expect(modrinthApiKey).toBe('REPL_MODRINTH_API_KEY');
+  });
+
+  it('are set to their values when they are overridden', async () => {
+    process.env.CURSEFORGE_API_KEY = 'cf-key';
+    process.env.MODRINTH_API_KEY = 'mr-key';
+    const { curseForgeApiKey, modrinthApiKey } = await import('./env.js');
+    expect(curseForgeApiKey).toBe('cf-key');
+    expect(modrinthApiKey).toBe('mr-key');
+  });
+});
