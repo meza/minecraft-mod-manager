9917eca72ca2071c26cf655d134408e5112943dc
meza <meza@meza.hu>
2022-10-03 17:15:42 +0100

    chore: fixed wrong deps

--- src/repositories/modrinth/index.ts
+++ src/repositories/modrinth/index.ts
@@ -1,4 +1,4 @@
-import { RemoteModDetails, Platform, ReleaseType } from '../../lib/modlist.types.js';
+import { Platform, ReleaseType, RemoteModDetails } from '../../lib/modlist.types.js';
 import { version } from '../../version.js';
 import { modrinthApiKey } from '../../env.js';
 import { CouldNotFindModException } from '../../errors/CouldNotFindModException.js';
@@ -9,19 +9,24 @@ interface Hash {
   sha512: string;
 }
 
-interface ModrinthFile {
+export interface ModrinthFile {
   hashes: Hash;
   url: string;
   filename: string;
 }
 
-interface ModrinthVersion {
+export interface ModrinthVersion {
   name: string;
   loaders: string[];
   game_versions: string[];
   date_published: string;
   version_type: ReleaseType;
   files: ModrinthFile[];
+}
+
+interface ModrinthMod {
+  name: string,
+  versions: ModrinthVersion[]
 }
 
 const apiHeaders = {
@@ -32,24 +37,20 @@ const apiHeaders = {
 
 const getName = async (projectId: string): Promise<string> => {
   const url = `https://api.modrinth.com/v2/project/${projectId}`;
-  const modDetailsRequest = await fetch(url, {
+  const modInfoRequest = await fetch(url, {
     headers: apiHeaders
   });
 
-  if (modDetailsRequest.status !== 200) {
+  if (modInfoRequest.status !== 200) {
     throw new CouldNotFindModException(projectId, Platform.MODRINTH);
   }
 
-  const modDetails = await modDetailsRequest.json();
-  return modDetails.title;
+  const modInfo = await modInfoRequest.json();
+  return modInfo.title;
 };
 
-export const getMod = async (
-  projectId: string,
-  allowedReleaseTypes: ReleaseType[],
-  allowedGameVersion: string,
-  loader: string,
-  allowFallback: boolean): Promise<RemoteModDetails> => {
+const getModDetails = async (projectId: string): Promise<ModrinthMod> => {
+  const name = await getName(projectId);
   const url = `https://api.modrinth.com/v2/project/${projectId}/version`;
 
   const modDetailsRequest = await fetch(url, {
@@ -62,31 +63,52 @@ export const getMod = async (
 
   const modVersions = await modDetailsRequest.json() as ModrinthVersion[];
 
-  const potentialFiles = modVersions
+  return {
+    versions: modVersions,
+    name: name
+  };
+};
+
+export const getMod = async (
+  projectId: string,
+  allowedReleaseTypes: ReleaseType[],
+  allowedGameVersion: string,
+  loader: string,
+  allowFallback: boolean): Promise<RemoteModDetails> => {
+
+  const { name, versions } = await getModDetails(projectId);
+  const potentialFiles = versions
     .filter((version) => {
       return version.loaders.map((origLoader: string) => origLoader.toLowerCase()).includes(loader.toLowerCase());
     })
     .filter((version) => {
-      const hasPerfectMatch = version.game_versions.includes(allowedGameVersion);
-      if (!hasPerfectMatch && allowFallback) {
-        const gameVersionsWithOnlyMajorAndMinor = version.game_versions.map((gameVersion) => {
-          const [major, minor] = gameVersion.split('.');
-          return `${major}.${minor}`;
-        });
-
-        const [major, minor] = allowedGameVersion.split('.');
-
-        return gameVersionsWithOnlyMajorAndMinor.includes(`${major}.${minor}`);
-      }
-      return hasPerfectMatch;
+      return allowedReleaseTypes.includes(version.version_type);
     })
     .filter((version) => {
-      return allowedReleaseTypes.includes(version.version_type);
+      if (version.game_versions.includes(allowedGameVersion)) {
+        return true;
+      }
+
+      if (allowFallback) {
+
+        const [major, minor, patch] = allowedGameVersion.split('.').map((num) => parseInt(num, 10));
+
+        if (patch && patch > 1) {
+          if (version.game_versions.includes(`${major}.${minor}.${patch - 1}`)) {
+            return true;
+          }
+        }
+
+        if (version.game_versions.includes(`${major}.${minor}`)) {
+          return true;
+        }
+      }
+
+      return false;
     })
     .sort((a, b) => {
       return a.date_published < b.date_published ? 1 : -1;
-    })
-  ;
+    });
 
   if (potentialFiles.length === 0) {
     throw new NoRemoteFileFound(projectId, Platform.MODRINTH);
@@ -95,7 +117,7 @@ export const getMod = async (
   const latestFile = potentialFiles[0];
 
   const modData: RemoteModDetails = {
-    name: await getName(projectId),
+    name: name,
     fileName: latestFile.files[0].filename,
     releaseDate: latestFile.date_published,
     hash: latestFile.files[0].hashes.sha1,
