a84e70171e3bcca20da20ce80510c04ec8b9e530
meza <meza@meza.hu>
2022-10-07 14:49:46 +0100

    chore: fixing release

--- scripts/binaries.sh
+++ scripts/binaries.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=$1
+
 mkdir -p dist/pkg/linux
 mkdir -p dist/pkg/win
 mkdir -p dist/pkg/macos
@@ -7,3 +9,7 @@ mkdir -p dist/pkg/macos
 mv dist/pkg/mmm-linux dist/pkg/linux/mmm
 mv dist/pkg/mmm-macos dist/pkg/macos/mmm
 mv dist/pkg/mmm-win.exe dist/pkg/win/mmm.exe
+
+zip -j "dist/minecraft-mod-manager-${VERSION}-linux.zip" dist/pkg/linux/mmm
+zip -j "dist/minecraft-mod-manager-${VERSION}-macos.zip" dist/pkg/macos/mmm
+zip -j "dist/minecraft-mod-manager-${VERSION}-windows.zip" dist/pkg/win/mmm.exe
