6c6f5a821941b2ccd66a183d64ad9ed7f291d3c0
meza <211977+meza@users.noreply.github.com>
2024-08-21 23:31:20 +0100

    chore: adding tests for the environment util

---
+++ internal/environment/environment_test.go
@@ -0,0 +1,52 @@
+package environment
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestModrinthApiKey(t *testing.T) {
+	t.Run("environment variable set", func(t *testing.T) {
+		expected := "test_modrinth_api_key"
+		os.Setenv("MODRINTH_API_KEY", expected)
+		defer os.Unsetenv("MODRINTH_API_KEY")
+
+		actual := ModrinthApiKey()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("environment variable not set", func(t *testing.T) {
+		os.Unsetenv("MODRINTH_API_KEY")
+
+		expected := "REPL_MODRINTH_API_KEY"
+		actual := ModrinthApiKey()
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestCurseforgeApiKey(t *testing.T) {
+	t.Run("environment variable set", func(t *testing.T) {
+		expected := "test_curseforge_api_key"
+		os.Setenv("CURSEFORGE_API_KEY", expected)
+		defer os.Unsetenv("CURSEFORGE_API_KEY")
+
+		actual := CurseforgeApiKey()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("environment variable not set", func(t *testing.T) {
+		os.Unsetenv("CURSEFORGE_API_KEY")
+
+		expected := "REPL_CURSEFORGE_API_KEY"
+		actual := CurseforgeApiKey()
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestAppVersion(t *testing.T) {
+	expected := "REPL_VERSION"
+	actual := AppVersion()
+	assert.Equal(t, expected, actual)
+}
